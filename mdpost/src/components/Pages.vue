f869a460bde9be77edd0735e682d667d680bab4f
yui <yuikita21@gmail.com>
2018-10-27 19:55:17 +0900

    fix

---
+++ mdpost/src/components/Pages.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    hi
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pages',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
