4938186612cb8afd4381c867c05e88b91d676c00
yui <yuikita21@gmail.com>
2018-10-29 20:39:47 +0900

    fix

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var app = express();
+var history = require('connect-history-api-fallback');
+
 var path = require('path');
 var serveStatic = require('serve-static');
-app = express();
-var history = require('connect-history-api-fallback');
+
+app.use(history());
+
 app.use(serveStatic(__dirname + "/mdpost/dist"));
-console.log(__dirname);
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
